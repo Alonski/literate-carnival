35d625bec7013a38f3ef2d4b7328ade70b38dc2c
Alon Bukai <alon@camilyo.com>
2019-05-24 15:30:29 +0300

    Clean up check_arr Various other code reorder actions

--- sudoku_threads.c
+++ sudoku_threads.c
@@ -30,7 +30,7 @@ int getMatrixFromFile(char *file_name);
 
 void init_threads(pthread_t *threads, char **arg);
 
-void create_threads(pthread_t *threads);
+void join_threads(pthread_t *threads);
 
 void free_arg_array(char **arg);
 
@@ -57,68 +57,20 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void check_matrix(pthread_t *threads) {
-    char *arg[MATRIX_SIZE * 3];
-    init_arg_array(arg);
-    init_threads(threads, arg);
-    create_threads(threads);
-    free_arg_array(arg);
-    int result = calculate_result();
-    if (result == MATRIX_SIZE * 3)
-        printf("solution is legal\n");
-    else
-        printf("solution is not legal\n");
-}
-
-void init_arg_array(char **arg) {
-    int i;
-    for (i = 0; i < MATRIX_SIZE * 3; i++) {
-        arg[i] = NULL;
-    }
-}
-
-int calculate_result() {
-    int i, res = 0;
-    for (i = 0; i < MATRIX_SIZE * 3; i++) {
-        if (result[i] > 0)
-            res++;
-    }
-    return res;
-}
-
-void free_arg_array(char **arg) {
-    int i;
-    for (i = 0; i < MATRIX_SIZE * 3; i++)
-        free(arg[i]);
-}
-
-void create_threads(pthread_t *threads) {
-    int i;
-    for (i = 0; i < MATRIX_SIZE * 3; i++) {
-        pthread_join(threads[i], NULL);
-    }
-}
-
-void init_threads(pthread_t *threads, char **arg) {
-    // Initialize threads
-    int i;
-    for (i = 0; i < MATRIX_SIZE * 3; i++) {
-        char tmp[MATRIX_SIZE];
-        arg[i] = realloc(arg[i], 6 * sizeof(char));            // initialize the "command" arguments
-
-        if (i < MATRIX_SIZE) {                                    // i between 0 and 9 --> check rows
-            arg[i] = strcpy(arg[i], ROW);
-        } else if (i >= MATRIX_SIZE && i < MATRIX_SIZE * 2) {    // i between 9 and 18 --> check column
-            arg[i] = strcpy(arg[i], COLUMN);
-        } else {                                                // i between 18 and 27 --> check sub matrix
-            arg[i] = strcpy(arg[i], SUB_MATRIX);
+void getMatrixFromTerminal() {
+    int i, j;
+    do {
+        // Get matrix from terminal
+        printf("Insert solution:\n");
+        for (i = 0; i < MATRIX_SIZE; i++) {
+            for (j = 0; j < MATRIX_SIZE; j++) {
+                scanf(" %d", &matrix[i][j]);
+                if (matrix[i][j] > 9 || matrix[i][j] < 1) {
+                    perror("Wrong input\n");
+                }
+            }
         }
-
-        arg[i] = strcat(arg[i], " ");
-        sprintf(tmp, "%i", i % 9);
-        arg[i] = strcat(arg[i], tmp);
-        pthread_create(&threads[i], NULL, check_arr, arg[i]);
-    }
+    } while (check_correct_input() < 0);
 }
 
 int getMatrixFromFile(char *file_name) {
@@ -138,20 +90,68 @@ int getMatrixFromFile(char *file_name) {
     return 1;
 }
 
-void getMatrixFromTerminal() {
-    int i, j;
-    do {
-        // Get matrix from terminal
-        printf("Insert solution:\n");
-        for (i = 0; i < MATRIX_SIZE; i++) {
-            for (j = 0; j < MATRIX_SIZE; j++) {
-                scanf(" %d", &matrix[i][j]);
-                if (matrix[i][j] > 9 || matrix[i][j] < 1) {
-                    perror("Wrong input\n");
-                }
-            }
+void check_matrix(pthread_t *threads) {
+    char *arg[MATRIX_SIZE * 3];
+    init_arg_array(arg);
+    init_threads(threads, arg);
+    join_threads(threads);
+    free_arg_array(arg);
+    int result = calculate_result();
+    if (result == MATRIX_SIZE * 3)
+        printf("solution is legal\n");
+    else
+        printf("solution is not legal\n");
+}
+
+void init_arg_array(char **arg) {
+    int i;
+    for (i = 0; i < MATRIX_SIZE * 3; i++) {
+        arg[i] = NULL;
+    }
+}
+
+void init_threads(pthread_t *threads, char **arg) {
+    // Initialize threads
+    int i;
+    for (i = 0; i < MATRIX_SIZE * 3; i++) {
+        char tmp[2];
+        arg[i] = realloc(arg[i], 6 * sizeof(char));            // initialize the "command" arguments
+
+        if (i < MATRIX_SIZE) {                                    // i between 0 and 9 --> check rows
+            arg[i] = strcpy(arg[i], ROW);
+        } else if (i >= MATRIX_SIZE && i < MATRIX_SIZE * 2) {    // i between 9 and 18 --> check column
+            arg[i] = strcpy(arg[i], COLUMN);
+        } else {                                                // i between 18 and 27 --> check sub matrix
+            arg[i] = strcpy(arg[i], SUB_MATRIX);
         }
-    } while (check_correct_input() < 0);
+
+        arg[i] = strcat(arg[i], " ");
+        sprintf(tmp, "%i", i % 9);
+        arg[i] = strcat(arg[i], tmp);
+        pthread_create(&threads[i], NULL, check_arr, arg[i]);
+    }
+}
+
+void join_threads(pthread_t *threads) {
+    int i;
+    for (i = 0; i < MATRIX_SIZE * 3; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+void free_arg_array(char **arg) {
+    int i;
+    for (i = 0; i < MATRIX_SIZE * 3; i++)
+        free(arg[i]);
+}
+
+int calculate_result() {
+    int i, res = 0;
+    for (i = 0; i < MATRIX_SIZE * 3; i++) {
+        if (result[i] > 0)
+            res++;
+    }
+    return res;
 }
 
 void *check_arr(void *arg) {
@@ -162,21 +162,17 @@ void *check_arr(void *arg) {
     int arr[MATRIX_SIZE];
     int check[MATRIX_SIZE] = {0};
     char *str = (char *) arg;
-    char *p = strtok(str, " ");
-    int num = 0, res = 1;
+    char *type = strtok(str, " ");
+    char *s_num = strtok(NULL, " ");
+    int num = atoi(s_num);
+    int res = 1;
 
-    if (strcmp(p, ROW) == 0) {
-        p = strtok(NULL, " ");
-        num = atoi(p);
+    if (strcmp(type, ROW) == 0) {
         get_row(arr, num);
-    } else if (strcmp(p, COLUMN) == 0) {
-        p = strtok(NULL, " ");
-        num = atoi(p);
+    } else if (strcmp(type, COLUMN) == 0) {
         get_col(arr, num);
         num += MATRIX_SIZE;
-    } else if (strcmp(p, SUB_MATRIX) == 0) {
-        p = strtok(NULL, " ");
-        num = atoi(p);
+    } else if (strcmp(type, SUB_MATRIX) == 0) {
         get_sub_matrix(arr, num);
         num += MATRIX_SIZE * 2;
     }
